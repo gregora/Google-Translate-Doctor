5498dc86485e832f48b639532b3638c38048cabe
Klemen <klemen.klopcic314@gmail.com>
2019-07-11 11:28:40 +0200

    Added voice and GUI.
    I downloaded library gTTs and made block of code to read the translated text.

--- index.py
+++ index.py
@@ -1,24 +1,21 @@
 from googletrans import Translator
 #from random import *
 import random
-translator = Translator()
+from gtts import gTTS
+import vlc
+import tkinter as tk
 
-lang = raw_input("Language: ")
 
-while True:
-
-    print("----------------------------------------------------------")
-
-    inp = raw_input("Your question: ");
-    print("Your Google Translate-Doctor will now answer your question")
-
+def translate():
+    global doctorAnswer, inp, e1
+    original = inp.get()
     trstr = []
     sendstr = ""
 
-    for c in inp:
+    for c in original:
         trstr.append(c)
 
-    for x in range(0, len(trstr)/2):
+    for x in range(0, len(trstr)//2):
         #if(x>0):
         sendstr = sendstr + trstr[2*x]
         sendstr = sendstr + trstr[2*x - 1] + " "
@@ -26,9 +23,46 @@ while True:
         if (random.randint(1, 8) == 4):
             sendstr += " "
         '''
-    #print(sendstr)
+
 
     #if len(sendstr) < 24:
     sendstr = "qe " * 10 + sendstr
+    print(sendstr)
+    endStr = translator.translate(sendstr, src = "so", dest = "en").text
+    print(doctorAnswer)
+    tts = gTTS(endStr, lang='en')
+    tts.save('answer.mp3')
+    player = vlc.MediaPlayer("answer.mp3")
+    player.play()
+    doctorAnswer.config(text = "Doctor answered:" + endStr)
+    doctorAnswer.grid(column = 0, row = 3)
 
-    print("\tThe Doctor replied: " + translator.translate(sendstr, src = "so", dest = lang).text)
+
+
+translator = Translator()
+root = tk.Tk()
+root.title("Doctor Google Translate")
+tts = gTTS('Hello patient!', lang='en')
+tts.save('hello.mp3')
+player = vlc.MediaPlayer("hello.mp3")
+player.play()
+
+'''
+w = tk.Label(root, text="Hello patient!").grid(row = 0)
+languageText = tk.Label(root, text = "Language:").grid(column = 0, row = 1)
+
+e1 = tk.Entry(root)
+e1.insert(10, "sl")
+e1.grid(column = 1, row = 1)
+lang = e1
+'''
+
+questionText = tk.Label(root, text = "Your question:").grid(column = 0, row = 2)
+inp = tk.Entry(root);
+inp.grid(column = 1, row = 2)
+doctorAnswer = tk.Label(root, text = "Doctor answered:")
+doctorAnswer.grid(column = 0, row = 3)
+b1 = tk.Button(root, text='Show', command=translate)
+b1.grid(column = 2, row = 2)
+root.mainloop()
+print("Your Google Translate-Doctor will now answer your question")
